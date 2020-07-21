39d350e40f9b37102e9249fa4b88efd8e63000ab
Joshua McMorris <mcmorrisj@spu.edu>
2020-07-20 19:08:35 -0700

    GLM and matrix confusion

--- GameEngineV2/Source/source.cpp
+++ GameEngineV2/Source/source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "window.h"
 #include "shaders.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 int main(void)
 {
@@ -37,6 +38,24 @@ int main(void)
     Shaders shaders = Shaders("Source/simpleVertexShader.vshader", "Source/simpleFragmentShader.fshader");
     GLuint programID = shaders.LoadShaders();
 
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float)100 / (float)100, 0.1f, 100.0f);
+
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View = glm::lookAt(
+        glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0, 0, 0), // and looks at the origin
+        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
     /* Loop until the user closes the window */
     while (!window.isClosing())
     {
@@ -63,6 +82,15 @@ int main(void)
         );
         // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+
+        // Get a handle for our "MVP" uniform
+        // Only during the initialisation
+        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+        // Send our transformation to the currently bound shader, in the "MVP" uniform
+        // This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
         glDisableVertexAttribArray(0);
     }
 
